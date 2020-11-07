58b954461a35c9dcf1d6431de708df1f2b34f7e5
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2020-11-07 19:48:37 +0100

    Incrémenter le score au clic du cookie

--- app/src/main/java/com/ynov/vernet/cookieclicker/MainActivity.java
+++ app/src/main/java/com/ynov/vernet/cookieclicker/MainActivity.java
@@ -1,14 +1,31 @@
 package com.ynov.vernet.cookieclicker;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewTemps, textViewScore;
+    private int score, temps = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Référence
+        textViewScore = findViewById(R.id.textViewScore);
+        textViewTemps = findViewById(R.id.textViewTemps);
+
+        // Au clic du cookie
+        findViewById(R.id.imageViewCookie).setOnClickListener(v -> {
+            // Incrémenter le score
+            score++;
+            textViewScore.setText("Score : " + score);
+        });
     }
 }
