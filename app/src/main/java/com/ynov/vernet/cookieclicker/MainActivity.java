b9bfa45e1e5f8c57e871b112c3d8e51bd6ad94b3
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2020-11-08 20:08:07 +0100

    Affiche la moyenne des clics et arrête le décompte si le temps atteint 0

--- app/src/main/java/com/ynov/vernet/cookieclicker/MainActivity.java
+++ app/src/main/java/com/ynov/vernet/cookieclicker/MainActivity.java
@@ -1,6 +1,9 @@
 package com.ynov.vernet.cookieclicker;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
@@ -10,7 +13,8 @@ public class MainActivity extends AppCompatActivity {
 
     TextView textViewTemps, textViewScore;
     private int score = 0;
-    private int temps = 10;
+    private int temps = 3;      /*Mettre 10*/
+    private CountDownTimer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,60 +25,70 @@ public class MainActivity extends AppCompatActivity {
         textViewScore = findViewById(R.id.textViewScore);
         textViewTemps = findViewById(R.id.textViewTemps);
 
-
         // Au clic du cookie
         findViewById(R.id.imageViewCookie).setOnClickListener(v -> {
 
             // Au 1er clic de l'image
-            if (score == 0)
-                decrementerChrono();
+            if (score == 0) {
+                // Démarrer le compte à rebours
+                timer = new CountDownTimer(1000, 20) {
 
-            // Incrémenter le score
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        try {
+                            decrementerChrono();
+                        } catch (Exception e) {
+                            Log.e("Erreur", "Erreur : " + e.toString());
+                        }
+                    }
+                }.start();
+            }
+
+            // Incrémenter le score à chaques clics
             incrementerScore();
         });
     }
 
     public void decrementerChrono() {
-        Thread thread = new Thread() {
-            @Override
-            public void run() {
-                try {
-                    while (!isInterrupted()) {
-                        Thread.sleep(1000);
-                        runOnUiThread(() -> {
-                            // Décrémenter le chrono
-                            temps--;
-                            textViewTemps.setText("Temps : " + temps + " s");
+        temps--;
+        textViewTemps.setText("Temps : " + temps + " s");
 
-                            // Si le temps atteint 0
-                            if (temps == 0) {
-                                // Arrêter le Thread
+        // Si le temps atteint 0
+        if (temps <= 0) {
+            // Arrêter le compte à rebours
+            timer.cancel();
 
-                                // Faire une moyenne des clics
-                                int moyenneClicks = score / 10;
-
-                                // Afficher la moyenne
-                                AlertDialog alertDialog = new AlertDialog.Builder(getApplicationContext())
-                                        .setIcon(android.R.drawable.ic_dialog_alert)
-                                        .setTitle("Titre")
-                                        .setMessage(moyenneClicks + " clics / secondes")
-                                        .setPositiveButton("Oui", (dialogInterface, i) -> {
-                                        })
-                                        .show();
-                                alertDialog.setCanceledOnTouchOutside(false);
-
-                            }
-                        });
-                    }
-                } catch (InterruptedException e) {
-                }
-            }
-        };
-        thread.start();
+            // Afficher les résultats
+            resultat();
+        }
+        else
+            // Continuer le compte à rebours
+            timer.start();
     }
 
     public void incrementerScore() {
         score++;
         textViewScore.setText("Score : " + score);
+    }
+
+    public void resultat() {
+
+        // Faire une moyenne des clics
+        double moyenneClicks = score / 10;
+
+        // Afficher la moyenne
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Terminé !")
+                .setMessage(moyenneClicks + " clics / secondes")
+                .setPositiveButton("Ok", (dialogInterface, i) -> {
+                })
+                .show();
+        alertDialog.setCanceledOnTouchOutside(false);
     }
 }
